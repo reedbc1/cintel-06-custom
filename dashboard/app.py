321c127b7627b4260e04df7c9c21a079ea0ba3d6
Brendan <reedbc1@gmail.com>
2024-04-11 18:43:41 -0500

    update

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,7 +5,7 @@ from shinywidgets import render_plotly
 import pandas as pd
 import plotly.graph_objects as go
 
-df = pd.read_csv('nvidia_stock_prices.csv')
+df = pd.read_csv('/nvidia_stock_prices.csv')
 
 # sidebar
 ui.page_opts(title="Nvidia Stock Prices", fillable=True)
