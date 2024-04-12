9a4c9ba483a2a6a34ae98fcc5cfffc9cd9122375
Brendan <reedbc1@gmail.com>
2024-04-11 23:10:33 -0500

    updated docs folder

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,10 +2,21 @@
 from shiny import reactive, render, req
 from shiny.express import input, ui
 from shinywidgets import render_plotly
+from faicons import icon_svg
 import pandas as pd
 import plotly.graph_objects as go
 import yfinance as yf
 
+# getting icons for value cards
+# From https://icons.getbootstrap.com/icons/piggy-bank/
+arrow_down = ui.HTML(
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#3c5faa" d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>'
+)
+arrow_up = ui.HTML(
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#3c5faa" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>'
+)
+
+### Getting Nvidia finance data
 # Ticker symbol for Nvidia
 ticker_symbol = "NVDA"
 
@@ -46,26 +57,31 @@ with ui.navset_pill(id="tab"):
 
     with ui.nav_panel("Stock chart"):
 
-        with ui.layout_columns():
+        with ui.layout_column_wrap(fill=False):
+            
+            
 
-            with ui.card():
-                ui.card_header("Highest Price")
-                @render.text
+            with ui.value_box(showcase = arrow_up):
+                "Highest Price"
+                
+                @render.ui
                 def get_highest_price():
                     df = filtered_data()
                     highest_price = round(df["High"].max(), 2)
                     return f"{highest_price}"
-                    
-            with ui.card():
-                ui.card_header("Lowest Price")
+                
+            with ui.value_box(showcase = arrow_down):
+                "Lowest Price"
+
                 @render.text
                 def get_lowest_price():
                     df = filtered_data()
                     lowest_price = round(df["Low"].min(), 2)
                     return f"{lowest_price}"
 
-            with ui.card():
-                ui.card_header("% Change")
+            with ui.value_box(showcase=icon_svg("percent")):
+                "Percent Change"
+
                 @render.text
                 def get_pct_change():
                     df = filtered_data()
@@ -85,6 +101,8 @@ with ui.navset_pill(id="tab"):
 
                 fig.update_layout(xaxis_rangeslider_visible=False)
                 return fig
+
+# add area for latest data
 
 # reactive calc
 @reactive.calc
