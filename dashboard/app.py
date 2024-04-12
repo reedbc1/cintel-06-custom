3d5bc479ebe755a06a77a1f5f4624828968c70ae
reedbc1 <101766219+reedbc1@users.noreply.github.com>
2024-04-12 07:22:32 -0500

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import pandas as pd
 import plotly.graph_objects as go
 import yfinance as yf
@@ -16,33 +17,17 @@ arrow_up = ui.HTML(
     '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#3c5faa" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>'
 )
 
-### Getting Nvidia finance data
-# Ticker symbol for Nvidia
-ticker_symbol = "NVDA"
+infile = Path(__file__).parent / "nvidia_stock_prices.csv"
+df = pd.read_csv(infile)
 
-# Start date (5 years ago from today)
-start_date = pd.Timestamp.now() - pd.DateOffset(years=5)
-
-# End date (today)
-end_date = pd.Timestamp.now()
-
-# Fetching stock data from Yahoo Finance
-df = yf.download(ticker_symbol, start=start_date, end=end_date)
-
-# reset index so that date is also a column
-df.reset_index(inplace=True)
-
-# change items in date column to strings
-df['Date'] = df['Date'].astype(str)
+start_date = df["Date"].min()
+end_date = df["Date"].max()
 
 # sidebar
 ui.page_opts(title="Nvidia Stock Prices", fillable=True)
 
 with ui.sidebar():
     ui.p("App selects data for Nvidia stock from the last 5 years.")
-
-    start_date = df['Date'].min()
-    end_date = df['Date'].max()
 
     ui.input_date_range("daterange", "Select dates", start = start_date, end = end_date)
 
@@ -58,9 +43,6 @@ with ui.navset_pill(id="tab"):
     with ui.nav_panel("Stock chart"):
 
         with ui.layout_column_wrap(fill=False):
-            
-            
-
             with ui.value_box(showcase = arrow_up):
                 "Highest Price"
                 
@@ -109,16 +91,13 @@ with ui.navset_pill(id="tab"):
 def filtered_data():
     req(input.daterange())
     
-    # convert string to datetime
-    dates_series = df["Date"] 
-    dates_series = pd.to_datetime(dates_series)
+    date_values = pd.to_datetime(df['Date'])
 
     # define start and end date
     start_date = pd.to_datetime(input.daterange()[0])
     end_date = pd.to_datetime(input.daterange()[1])
 
     # Check if dates fall within the range
-    filtered_df = df[(dates_series >= start_date) & (dates_series <= end_date)]
+    filtered_df = df[(date_values >= start_date) & (date_values <= end_date)]
 
     return filtered_df
-
